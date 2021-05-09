8b7da56d7f314544a87e8ba42ac5acdf6d4b9b7e
bobsfriend12 <calebsethherring+github@gmail.com>
2021-05-09 16:31:03 -0500

    Merge branch 'master' of https://githu.com/bobsfriend12/google-meet-remove-controls

--- extension/background.js
+++ extension/background.js
@@ -1,116 +1,141 @@
-const clickHandler = result => {
-	let controlsType = "hideControls";
-	let namesType = "hideNames";
-	let bothType = "hideBoth";
+// const clickHandler = result => {
+// 	let controlsType = "hideControls";
+// 	let namesType = "hideNames";
+// 	let bothType = "hideBoth";
 
+// 	chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+// 		chrome.tabs.sendMessage(tabs[0].id, { type: "query" }, response => {
+// 			if (response.controlsHidden === true) {
+// 				controlsType = "unhideControls";
+// 			}
+// 			if (response.namesHidden === true) {
+// 				namesType = "unhideNames";
+// 			}
+// 			if (
+// 				response.controlsHidden === true &&
+// 				response.namesHidden === true
+// 			) {
+// 				bothType = "unhideBoth";
+// 			}
+// 		});
+// 	});
+
+// 	//#region
+// 	if (result.menuItemId === "51") {
+// 		chrome.tabs.query(
+// 			{ active: true, currentWindow: true },
+// 			function (tabs) {
+// 				chrome.tabs.sendMessage(
+// 					tabs[0].id,
+// 					{ type: controlsType },
+// 					function (response) {}
+// 				);
+// 			}
+// 		);
+// 	} else if (result.menuItemId === "52") {
+// 		chrome.tabs.query(
+// 			{ active: true, currentWindow: true },
+// 			function (tabs) {
+// 				chrome.tabs.sendMessage(
+// 					tabs[0].id,
+// 					{ type: namesType },
+// 					function (response) {}
+// 				);
+// 			}
+// 		);
+// 	} else if (result.menuItemId === "53") {
+// 		chrome.tabs.query(
+// 			{ active: true, currentWindow: true },
+// 			function (tabs) {
+// 				chrome.tabs.sendMessage(
+// 					tabs[0].id,
+// 					{ type: bothType },
+// 					function (response) {}
+// 				);
+// 			}
+// 		);
+// 	}
+// 	//#endregion
+// };
+
+// function updateContextMenus(controlsHidden, namesHidden) {
+// 	if (controlsHidden === true && namesHidden === true) {
+// 		chrome.contextMenus.update("51", { title: "Unhide Controls" });
+// 		chrome.contextMenus.update("52", { title: "Unhide Names" });
+// 		chrome.contextMenus.update("53", { title: "Unhide Both" });
+// 	} else if (controlsHidden === true && namesHidden === false) {
+// 		chrome.contextMenus.update("51", { title: "Unhide Controls" });
+// 		chrome.contextMenus.update("52", { title: "Hide Names" });
+// 		chrome.contextMenus.update("53", { title: "Hide Both" });
+// 	} else if (controlsHidden === false && namesHidden === true) {
+// 		chrome.contextMenus.update("51", { title: "Hide Controls" });
+// 		chrome.contextMenus.update("52", { title: "Unhide Names" });
+// 		chrome.contextMenus.update("53", { title: "Hide Both" });
+// 	} else if (controlsHidden === true && namesHidden === false) {
+// 		chrome.contextMenus.update("51", { title: "Hide Controls" });
+// 		chrome.contextMenus.update("52", { title: "Hide Names" });
+// 		chrome.contextMenus.update("53", { title: "Hide Both" });
+// 	}
+// }
+
+// chrome.contextMenus.create({
+// 	title: "Google Meet Remove Controls",
+// 	contexts: ["page"],
+// 	id: "50",
+// 	documentUrlPatterns: ["*://meet.google.com/*"],
+// });
+
+// chrome.contextMenus.create({
+// 	title: "Hide Controls",
+// 	contexts: ["page"],
+// 	id: "51",
+// 	parentId: "50",
+// });
+
+// chrome.contextMenus.create({
+// 	title: "Hide Names",
+// 	contexts: ["page"],
+// 	id: "52",
+// 	parentId: "50",
+// });
+
+// chrome.contextMenus.create({
+// 	title: "Hide Both",
+// 	contexts: ["page"],
+// 	id: "53",
+// 	parentId: "50",
+// });
+
+// chrome.contextMenus.onClicked.addListener(clickHandler);
+
+//#endregion
+
+//#region contentScript
+
+function injectContentScript() {
 	chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-		chrome.tabs.sendMessage(tabs[0].id, { type: "query" }, response => {
-			if (response.controlsHidden === true) {
-				controlsType = "unhideControls";
-			}
-			if (response.namesHidden === true) {
-				namesType = "unhideNames";
-			}
-			if (
-				response.controlsHidden === true &&
-				response.namesHidden === true
-			) {
-				bothType = "unhideBoth";
-			}
+		console.log(tabs[0].id);
+		const target = tabs[0].id;
+		chrome.scripting.executeScript({
+			target: { tabId: target },
+			files: ["content.js"],
 		});
 	});
-
-	//#region
-	if (result.menuItemId === "51") {
-		chrome.tabs.query(
-			{ active: true, currentWindow: true },
-			function (tabs) {
-				chrome.tabs.sendMessage(
-					tabs[0].id,
-					{ type: controlsType },
-					function (response) {}
-				);
-			}
-		);
-	} else if (result.menuItemId === "52") {
-		chrome.tabs.query(
-			{ active: true, currentWindow: true },
-			function (tabs) {
-				chrome.tabs.sendMessage(
-					tabs[0].id,
-					{ type: namesType },
-					function (response) {}
-				);
-			}
-		);
-	} else if (result.menuItemId === "53") {
-		chrome.tabs.query(
-			{ active: true, currentWindow: true },
-			function (tabs) {
-				chrome.tabs.sendMessage(
-					tabs[0].id,
-					{ type: bothType },
-					function (response) {}
-				);
-			}
-		);
-	}
-	//#endregion
-};
-
-function updateContextMenus(controlsHidden, namesHidden) {
-	if (controlsHidden === true && namesHidden === true) {
-		chrome.contextMenus.update("51", { title: "Unhide Controls" });
-		chrome.contextMenus.update("52", { title: "Unhide Names" });
-		chrome.contextMenus.update("53", { title: "Unhide Both" });
-	} else if (controlsHidden === true && namesHidden === false) {
-		chrome.contextMenus.update("51", { title: "Unhide Controls" });
-		chrome.contextMenus.update("52", { title: "Hide Names" });
-		chrome.contextMenus.update("53", { title: "Hide Both" });
-	} else if (controlsHidden === false && namesHidden === true) {
-		chrome.contextMenus.update("51", { title: "Hide Controls" });
-		chrome.contextMenus.update("52", { title: "Unhide Names" });
-		chrome.contextMenus.update("53", { title: "Hide Both" });
-	} else if (controlsHidden === true && namesHidden === false) {
-		chrome.contextMenus.update("51", { title: "Hide Controls" });
-		chrome.contextMenus.update("52", { title: "Hide Names" });
-		chrome.contextMenus.update("53", { title: "Hide Both" });
-	}
 }
 
-chrome.contextMenus.create({
-	title: "Google Meet Remove Controls",
-	contexts: ["page"],
-	id: "50",
-	documentUrlPatterns: ["*://meet.google.com/*"],
-});
+//#endregion
 
-chrome.contextMenus.create({
-	title: "Hide Controls",
-	contexts: ["page"],
-	id: "51",
-	parentId: "50",
-});
-
-chrome.contextMenus.create({
-	title: "Hide Names",
-	contexts: ["page"],
-	id: "52",
-	parentId: "50",
-});
-
-chrome.contextMenus.create({
-	title: "Hide Both",
-	contexts: ["page"],
-	id: "53",
-	parentId: "50",
-});
-
-chrome.contextMenus.onClicked.addListener(clickHandler);
-
+//#region message handler
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	if (request.type === "updateContext") {
 		console.log(request);
 		updateContextMenus(request.controlsHidden, request.namesHidden);
 	}
 });
+
+chrome.webNavigation.onCompleted.addListener(details => {
+	console.log(details);
+	injectContentScript();
+});
+
+//#endregion
