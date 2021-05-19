4284414a9774362bd6f49fadc4a3f9749f114695
bobsfriend12 <calebsethherring+github@gmail.com>
2021-05-19 14:48:39 -0500

    removed functionality if you arn't in a meet

--- extension/popup.js
+++ extension/popup.js
@@ -3,186 +3,202 @@ const names = document.getElementById("names");
 const both = document.getElementById("both");
 let controlsHidden, namesHidden;
 
-chrome.runtime.sendMessage({type: 'injectContent'}, () => {});
+chrome.runtime.sendMessage({type: 'injectContent'}, () => {
+	
+	
+	function showBtns() {
+		const btnContainer = document.getElementsByClassName('btn-container')[0];
+		const errContainer = document.getElementsByClassName('error-container')[0];
 
-function updateBtns() {
-	if (controlsHidden === true && namesHidden === true) {
-		controls.innerText = "Unhide Controls";
-		names.innerText = "Unhide Names";
-		both.innerText = "Unhide Both";
-	} else if (controlsHidden === true && namesHidden === false) {
-		controls.innerText = "Unhide Controls";
-	} else if (controlsHidden === false && namesHidden === true) {
-		names.innerText = "Unhide Names";
+		btnContainer.classList.toggle('hide');
+		errContainer.classList.toggle('hide');
 	}
-}
 
-chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-	chrome.tabs.sendMessage(tabs[0].id, { type: "query" }, response => {
-		controlsHidden = response.controlsHidden;
-		namesHidden = response.namesHidden;
-		updateBtns();
+	function updateBtns(url) {
+
+		if(url.indexOf('meet.google.com/') >= 0 && url.length >= 35) {
+			showBtns();
+		}
+
+
+		if (controlsHidden === true && namesHidden === true) {
+			controls.innerText = "Unhide Controls";
+			names.innerText = "Unhide Names";
+			both.innerText = "Unhide Both";
+		} else if (controlsHidden === true && namesHidden === false) {
+			controls.innerText = "Unhide Controls";
+		} else if (controlsHidden === false && namesHidden === true) {
+			names.innerText = "Unhide Names";
+		}
+	}
+
+	chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+		chrome.tabs.sendMessage(tabs[0].id, { type: "query" }, response => {
+			controlsHidden = response.controlsHidden;
+			namesHidden = response.namesHidden;
+			updateBtns(tabs[0].url);
+		});
 	});
-});
 
-controls.addEventListener("click", e => {
-	e.preventDefault();
-	let type;
+	controls.addEventListener("click", e => {
+		e.preventDefault();
+		let type;
 
-	if (controls.innerText === "Unhide Controls") {
-		type = "unhideControls";
-	} else {
-		type = "hideControls";
-	}
+		if (controls.innerText === "Unhide Controls") {
+			type = "unhideControls";
+		} else {
+			type = "hideControls";
+		}
 
-	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-		chrome.tabs.sendMessage(
-			tabs[0].id,
-			{ type: type },
-			function (response) {
-				if (
-					response.controlsHidden === true &&
-					response.namesHidden === true
-				) {
-					controls.innerText = "Unhide Controls";
-					names.innerText = "Unhide Names";
-					both.innerText = "Unhide Both";
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			chrome.tabs.sendMessage(
+				tabs[0].id,
+				{ type: type },
+				function (response) {
+					if (
+						response.controlsHidden === true &&
+						response.namesHidden === true
+					) {
+						controls.innerText = "Unhide Controls";
+						names.innerText = "Unhide Names";
+						both.innerText = "Unhide Both";
 
-					chrome.storage.sync.set;
+						chrome.storage.sync.set;
 
-					return;
-				} else if (
-					response.controlsHidden === true &&
-					response.namesHidden === false
-				) {
-					controls.innerText = "Unhide Controls";
-					names.innerText = "Hide Names";
-					both.innerText = "Hide Both";
-					return;
-				} else if (
-					response.controlsHidden === false &&
-					response.namesHidden === true
-				) {
-					controls.innerText = "Hide Controls";
-					names.innerText = "Unhide Names";
-					both.innerText = "Hide Both";
-					return;
-				} else if (
-					response.controlsHidden === false &&
-					response.namesHidden === false
-				) {
-					controls.innerText = "Hide Controls";
-					names.innerText = "Hide Names";
-					both.innerText = "Hide Both";
-					return;
+						return;
+					} else if (
+						response.controlsHidden === true &&
+						response.namesHidden === false
+					) {
+						controls.innerText = "Unhide Controls";
+						names.innerText = "Hide Names";
+						both.innerText = "Hide Both";
+						return;
+					} else if (
+						response.controlsHidden === false &&
+						response.namesHidden === true
+					) {
+						controls.innerText = "Hide Controls";
+						names.innerText = "Unhide Names";
+						both.innerText = "Hide Both";
+						return;
+					} else if (
+						response.controlsHidden === false &&
+						response.namesHidden === false
+					) {
+						controls.innerText = "Hide Controls";
+						names.innerText = "Hide Names";
+						both.innerText = "Hide Both";
+						return;
+					}
 				}
-			}
-		);
+			);
+		});
 	});
-});
 
-names.addEventListener("click", e => {
-	e.preventDefault();
-	let type;
+	names.addEventListener("click", e => {
+		e.preventDefault();
+		let type;
 
-	if (names.innerText === "Unhide Names") {
-		type = "unhideNames";
-	} else {
-		type = "hideNames";
-	}
+		if (names.innerText === "Unhide Names") {
+			type = "unhideNames";
+		} else {
+			type = "hideNames";
+		}
 
-	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-		chrome.tabs.sendMessage(
-			tabs[0].id,
-			{ type: type },
-			function (response) {
-				if (
-					response.controlsHidden === true &&
-					response.namesHidden === true
-				) {
-					controls.innerText = "Unhide Controls";
-					names.innerText = "Unhide Names";
-					both.innerText = "Unhide Both";
-					return;
-				} else if (
-					response.controlsHidden === true &&
-					response.namesHidden === false
-				) {
-					controls.innerText = "Unhide Controls";
-					names.innerText = "Hide Names";
-					both.innerText = "Hide Both";
-					return;
-				} else if (
-					response.controlsHidden === false &&
-					response.namesHidden === true
-				) {
-					controls.innerText = "Hide Controls";
-					names.innerText = "Unhide Names";
-					both.innerText = "Hide Both";
-					return;
-				} else if (
-					response.controlsHidden === false &&
-					response.namesHidden === false
-				) {
-					controls.innerText = "Hide Controls";
-					names.innerText = "Hide Names";
-					both.innerText = "Hide Both";
-					return;
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			chrome.tabs.sendMessage(
+				tabs[0].id,
+				{ type: type },
+				function (response) {
+					if (
+						response.controlsHidden === true &&
+						response.namesHidden === true
+					) {
+						controls.innerText = "Unhide Controls";
+						names.innerText = "Unhide Names";
+						both.innerText = "Unhide Both";
+						return;
+					} else if (
+						response.controlsHidden === true &&
+						response.namesHidden === false
+					) {
+						controls.innerText = "Unhide Controls";
+						names.innerText = "Hide Names";
+						both.innerText = "Hide Both";
+						return;
+					} else if (
+						response.controlsHidden === false &&
+						response.namesHidden === true
+					) {
+						controls.innerText = "Hide Controls";
+						names.innerText = "Unhide Names";
+						both.innerText = "Hide Both";
+						return;
+					} else if (
+						response.controlsHidden === false &&
+						response.namesHidden === false
+					) {
+						controls.innerText = "Hide Controls";
+						names.innerText = "Hide Names";
+						both.innerText = "Hide Both";
+						return;
+					}
 				}
-			}
-		);
+			);
+		});
 	});
-});
 
-both.addEventListener("click", e => {
-	e.preventDefault();
-	let type;
+	both.addEventListener("click", e => {
+		e.preventDefault();
+		let type;
 
-	if (both.innerText === "Unhide Both") {
-		type = "unhideBoth";
-	} else {
-		type = "hideBoth";
-	}
+		if (both.innerText === "Unhide Both") {
+			type = "unhideBoth";
+		} else {
+			type = "hideBoth";
+		}
 
-	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-		chrome.tabs.sendMessage(
-			tabs[0].id,
-			{ type: type },
-			function (response) {
-				if (
-					response.controlsHidden === true &&
-					response.namesHidden === true
-				) {
-					controls.innerText = "Unhide Controls";
-					names.innerText = "Unhide Names";
-					both.innerText = "Unhide Both";
-					return;
-				} else if (
-					response.controlsHidden === true &&
-					response.namesHidden === false
-				) {
-					controls.innerText = "Unhide Controls";
-					names.innerText = "Hide Names";
-					both.innerText = "Hide Both";
-					return;
-				} else if (
-					response.controlsHidden === false &&
-					response.namesHidden === true
-				) {
-					controls.innerText = "Hide Controls";
-					names.innerText = "Unhide Names";
-					both.innerText = "Hide Both";
-					return;
-				} else if (
-					response.controlsHidden === false &&
-					response.namesHidden === false
-				) {
-					controls.innerText = "Hide Controls";
-					names.innerText = "Hide Names";
-					both.innerText = "Hide Both";
-					return;
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			chrome.tabs.sendMessage(
+				tabs[0].id,
+				{ type: type },
+				function (response) {
+					if (
+						response.controlsHidden === true &&
+						response.namesHidden === true
+					) {
+						controls.innerText = "Unhide Controls";
+						names.innerText = "Unhide Names";
+						both.innerText = "Unhide Both";
+						return;
+					} else if (
+						response.controlsHidden === true &&
+						response.namesHidden === false
+					) {
+						controls.innerText = "Unhide Controls";
+						names.innerText = "Hide Names";
+						both.innerText = "Hide Both";
+						return;
+					} else if (
+						response.controlsHidden === false &&
+						response.namesHidden === true
+					) {
+						controls.innerText = "Hide Controls";
+						names.innerText = "Unhide Names";
+						both.innerText = "Hide Both";
+						return;
+					} else if (
+						response.controlsHidden === false &&
+						response.namesHidden === false
+					) {
+						controls.innerText = "Hide Controls";
+						names.innerText = "Hide Names";
+						both.innerText = "Hide Both";
+						return;
+					}
 				}
-			}
-		);
+			);
+		});
 	});
 });
